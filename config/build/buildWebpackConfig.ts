42c89ff092dcc91f91489b97191a1c580bba9b70
egorlitin <egorulitin@gmail.com>
2023-11-16 16:49:37 +0300

    add webpack-dev-server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,15 +1,17 @@
 import webpack from 'webpack';
-import path from 'path';
 import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(
   options: BuildOptions,
 ): webpack.Configuration {
   const {
     mode,
+    port,
+    isDev,
     paths: { entry, build, html },
   } = options;
   return {
@@ -25,5 +27,7 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(port) : undefined,
   };
 }
