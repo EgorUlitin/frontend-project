792d7b5e7850f4fb215e09087c2ca01b7804f79a
egorlitin <egorulitin@gmail.com>
2023-11-17 19:22:10 +0300

    add hook useTheme and light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './index.scss';
+import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to={'/'}>Main</Link>
       <Link to={'/about'}>About</Link>
+
+      <button onClick={toggleTheme}>Switch</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
