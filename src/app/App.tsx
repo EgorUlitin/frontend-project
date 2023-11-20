a4d2959dd9b119409229a731d5fa3d61bcff0462
egorlitin <egorulitin@gmail.com>
2023-11-20 14:31:40 +0300

    add  Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,18 @@
 import './styles/index.scss';
-import { Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
+      <Navbar />
 
       <button onClick={toggleTheme}>Switch</button>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,28 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Navbar.module.scss';
+import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+  className?: string;
+}
+
+const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={classNames(cls.links)}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+          to={'/'}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+          О Сайте
+        </AppLink>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
