24628f6ca23804e2459b485ebeddfc511a008d24
egorlitin <egorulitin@gmail.com>
2023-11-22 20:24:39 +0300

    add stylelint, i18n plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,46 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:react/recommended',
-		'airbnb',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	plugins: [
-		'react',
-		'@typescript-eslint',
-	],
-	rules: {
-		'react/jsx-indent': [2, 4],
-		'react/jsx-indent-props': [2, 4],
-		indent: [2, 4],
-		'react/react-in-jsx-scope': 'off',
-		'react/jsx-uses-react': 'off',
-		'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-		'react/require-default-props': 'off',
-		'react/function-component-definition': 'off',
-		'react/jsx-props-no-spreading': 'warn',
-		'import/no-unresolved': 'off',
-		'import/extensions': 'off',
-		'import/prefer-default-export': 'off',
-		'no-unused-vars': 'warn',
-		'no-shadow': 'off',
-		'import/no-extraneous-dependencies': 'off',
-		'no-underscore-dangle': 'off',
-	},
-	globals: {
-		__IS_DEV__: true,
-	},
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:i18next/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        'i18next',
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/require-default-props': 'off',
+        'react/function-component-definition': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'import/no-unresolved': 'off',
+        'import/extensions': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'no-shadow': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+    },
+    globals: {
+        __IS_DEV__: true,
+    },
 };
