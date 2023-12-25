72cac3211430619f38b839334f04a8ac51c8ac38
egorlitin <egorulitin@gmail.com>
2023-12-25 17:06:27 +0300

    add green theme and concurrently

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,7 +14,22 @@ export function useTheme(): UseThemeResult {
     const { theme, setTheme } = useContext(ThemeContext);
 
     const toggleTheme = () => {
-        const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+        let newTheme: Theme;
+
+        switch (theme) {
+        case Theme.DARK:
+            newTheme = Theme.LIGHT;
+            break;
+        case Theme.LIGHT:
+            newTheme = Theme.GREEN;
+            break;
+        case Theme.GREEN:
+            newTheme = Theme.DARK;
+            break;
+        default:
+            newTheme = Theme.LIGHT;
+            break;
+        }
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
         setTheme?.(newTheme);
         document.body.className = newTheme;
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,22 +2,32 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme, Theme } from 'app/providers/ThemeProvider';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import GreenIcon from 'shared/assets/icons/theme-green.svg';
 import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 import { memo } from 'react';
 
 interface ThemeSwitcherProps {
   className?: string;
 }
+const map = {
+    [Theme.DARK]: <LightIcon />,
+    [Theme.GREEN]: <DarkIcon />,
+    [Theme.LIGHT]: <GreenIcon />,
+
+};
 
 export const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
+
     return (
         <Button
             theme={ButtonTheme.CLEAR}
             className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
-            {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+
+            {/* {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />} */}
+            {map[theme]}
         </Button>
     );
 });
