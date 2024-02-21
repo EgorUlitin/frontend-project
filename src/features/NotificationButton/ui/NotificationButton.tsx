b7a3aa2bb10c6635979451823e02177b1aa9f26f
egorlitin <egorulitin@gmail.com>
2024-02-21 12:53:46 +0300

    add browerslist config, refactor drawer

--- src/features/NotificationButton/ui/NotificationButton.tsx
+++ src/features/NotificationButton/ui/NotificationButton.tsx
@@ -7,7 +7,6 @@ import { Popover } from '@/shared/ui/Popups';
 import { Icon } from '@/shared/ui/Icon/Icon';
 import NotificationIcon from '@/shared/assets/icons/notification-20-20.svg';
 import { Drawer } from '@/shared/ui/Drawer/Drawer';
-import { AnimationProvider } from '@/shared/lib/components/AnimationProvider';
 import cls from './NotificationButton.module.scss';
 
 interface NotificationButtonProps {
@@ -49,11 +48,9 @@ export const NotificationButton = memo(({ className }: NotificationButtonProps) 
             </BrowserView>
             <MobileView>
                 {trigger}
-                <AnimationProvider>
-                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                        <NotificationList />
-                    </Drawer>
-                </AnimationProvider>
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                    <NotificationList />
+                </Drawer>
             </MobileView>
         </div>
     );
