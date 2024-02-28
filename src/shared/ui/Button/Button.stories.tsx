2b3ab6001386e1bdf4aa2e03227929f85cb5d7ed
egorlitin <egorulitin@gmail.com>
2024-02-28 15:41:31 +0300

    add documentation for project

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import Button, { ButtonSize, ButtonTheme } from './Button';
+import { Button, ButtonSize, ButtonTheme } from './Button';
 import { Theme } from '@/shared/const/theme';
 
 const meta: Meta<typeof Button> = {
